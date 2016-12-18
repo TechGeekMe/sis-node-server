f1b8c8f5ba34f855bacec830a1177b6db5889bab
KidEinstein <anirudhshekh@gmail.com>
2016-12-18 20:11:52 +0530

    Factored out credentials

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 var express = require('express');
+var credentials = require('./credentials')
 require('express-mongoose');
 var models = require('./models/student.js')
 var routes = require('./routes');
 global.updatingList = new Array()
-mongoose.connect('mongodb://sis:***REMOVED***@***REMOVED***', function(err) {
+mongoose.connect(credentials.mongodb.uri, {user: credentials.mongodb.user, pass: credentials.mongodb.pass}, function(err) {
     if (err) throw err;
     console.log("Connected!")
 
